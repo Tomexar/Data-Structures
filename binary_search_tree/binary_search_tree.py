9b4fb16752960345d5ac8ad439f52832e7f16969
tomexar <tommyexar@gmail.com>
2019-09-18 16:52:19 -0600

    bianary search passes

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -10,13 +10,41 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value:
+      if not self.left:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    else:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
-    pass
+    if target == self.value:
+      return True
+    elif target < self.value:
+      if self.left:
+        return self.left.contains(target)
+      else:
+        return False
+    elif target > self.value:
+      if self.right:
+        return self.right.contains(target)
+      else:
+        return False
 
   def get_max(self):
-    pass
+    temp = self
+    while temp.right is not None:
+      temp = temp.right
+    return temp.value
 
   def for_each(self, cb):
-    pass
+    cb(self.value)
+
+    if self.left:
+      self.left.for_each(cb)
+    if self.right:
+      self.right.for_each(cb)
